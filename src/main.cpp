934554eb2f02022e813aa171e1f34e2a016977ad
David <davoltra@gmail.com>
2024-03-30 18:43:58 +0100

    change rotary to switch

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,11 @@
 #include <esp_log.h>
 #include <esp_timer.h>
 #include <pinout.h>
-#include <rotary.h>
 #include <led.h>
 #include <TB6612.h>
 #include <QRE1113.h>
 #include <VL53L0X.h>
+
 
 #define I2C_PORT I2C_NUM_0
 
@@ -16,7 +16,6 @@ static char TAG[] = "MAIN";
 
 extern "C" void app_main();
 
-ROTARY rotary(PIN_ROTARY_S1, PIN_ROTARY_S2, PIN_ROTARY_KEY);
 LED led_left(PIN_LED_L), led_front(PIN_LED_F), led_right(PIN_LED_R), led_back(PIN_LED_B);
 TB6612 motores(PIN_MOTOR_A_1, PIN_MOTOR_A_2, PIN_MOTOR_A_PWM, PIN_MOTOR_B_1, PIN_MOTOR_B_2, PIN_MOTOR_B_PWM);
 QRE1113 sensor_linea_i(PIN_QRE1113_L);
@@ -34,8 +33,6 @@ static TaskHandle_t core_B = NULL;
 void Init()
 {
     ESP_LOGI(TAG,"INIT");
-    ESP_LOGI(TAG,"ROTARY ENCODER");
-    rotary.Init();
     ESP_LOGI(TAG,"LEDS");
     led_left.Init(); 
     vTaskDelay(pdMS_TO_TICKS(50));
@@ -86,7 +83,7 @@ void Init_VL53L0X()
     }
     sensor_distancia_i.i2cMasterInit(PIN_SDA,PIN_SCL);
     if (!sensor_distancia_i.init()) {
-        ESP_LOGE(TAG, "Failed to initialize VL53L0X :(");
+        ESP_LOGE(TAG, "Failed to initialize VL53L0X I :(");
         while(1)
         {
             Leds_update(LED::BLINK_250ms, LED::OFF, LED::OFF, LED::OFF);
@@ -95,22 +92,34 @@ void Init_VL53L0X()
     sensor_distancia_i.setDeviceAddress(0x31);
     sensor_distancia_c.reset();
     sensor_distancia_d.reset();
+    vTaskDelay(pdMS_TO_TICKS(250));
     if (!sensor_distancia_c.init()) {
-        ESP_LOGE(TAG, "Failed to initialize VL53L0X :(");
+        ESP_LOGE(TAG, "Failed to initialize VL53L0X C :(");
         while(1)
         {
             Leds_update(LED::OFF, LED::BLINK_250ms, LED::OFF, LED::OFF);
         }
     }
-    sensor_distancia_c.setDeviceAddress(0x30);
+    sensor_distancia_c.setDeviceAddress(0x30);   
     sensor_distancia_d.reset();
     if (!sensor_distancia_d.init()) {
-        ESP_LOGE(TAG, "Failed to initialize VL53L0X :(");
+        ESP_LOGE(TAG, "Failed to initialize VL53L0X D :(");
         while(1)
         {
             Leds_update(LED::OFF, LED::OFF, LED::BLINK_250ms, LED::OFF);
         }
     }
+}
+
+void Init_Key()
+{
+    gpio_config_t io_conf = {};
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.pin_bit_mask = (1ULL << PIN_KEY);
+    io_conf.pull_down_en = GPIO_PULLDOWN_ENABLE;
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    gpio_config(&io_conf);
 }
 
 
@@ -146,95 +155,110 @@ bool CheckSensorLinea(QRE1113 sensor_linea)
 
 int8_t STATUS_TOF = 0;
 int8_t start = 0;
-int8_t selected_start_mode;
+int8_t selected_start_mode = 0;
+int ref_time = 0;
+int key_time = 0;
+bool key_status = 0;
+bool key_push = 0;
 void coreAThread(void *arg)
 {
     ESP_LOGE(TAG, "Iniciando CORE A");
 
-    while(rotary.Key())
-    { 
-//        ESP_LOGI(TAG,"CORE_A start: %u",rotary.Key());
-        uint8_t status_rotary = rotary.CheckEncoder();
-//        printf("case: %u \n", status_rotary);
-
-        switch (status_rotary)
-        {
-        case 0:
-            Leds_update(LED::ON, LED::OFF, LED::OFF, LED::OFF);
-        //    led_left.Update(LED::ON);
-        //    led_front.Update(LED::OFF);
-        //    led_right.Update(LED::OFF);
-        //    led_back.Update(LED::OFF);
-            selected_start_mode = 0;
-        break;
-        case 1:
-            Leds_update(LED::OFF, LED::ON, LED::OFF, LED::OFF);
-            selected_start_mode = 1;
-        break;
-        case 2:
-            Leds_update(LED::OFF, LED::OFF, LED::ON, LED::OFF);
-            selected_start_mode = 2;
-        break;
-        case 3:
-            Leds_update(LED::OFF, LED::OFF, LED::OFF, LED::ON);
-            selected_start_mode = 3;
-        break;
-        default:
-        break;
-        }
-    };
-
-    while(!rotary.Key())
+    while(1)
     {
-        if(selected_start_mode == 0){led_left.Update(LED::ON);}   else {led_left.Update(LED::BLINK_250ms);}
-        if(selected_start_mode == 1){led_front.Update(LED::ON);}   else {led_front.Update(LED::BLINK_250ms);}
-        if(selected_start_mode == 2){led_right.Update(LED::ON);}   else {led_right.Update(LED::BLINK_250ms);}
-        if(selected_start_mode == 3){led_back.Update(LED::ON);}   else {led_back.Update(LED::BLINK_250ms);}
-    }
+        bool key_status = gpio_get_level(PIN_KEY);
 
-    vTaskDelay(pdMS_TO_TICKS(tiempo_espera_inicio));    //5seg
-    start = 1;
-
-    switch(selected_start_mode)
-    {  
-        case 0:                             //LEFT
-            motores.Update(TB6612::RIGHT);     
-            vTaskDelay(pdMS_TO_TICKS(250));
-        break;
-        case 1:                             //FRONT
-            motores.Update(TB6612::FWD);     
-        break;
-        case 2:                             //RIGHT
-            motores.Update(TB6612::LEFT);     
-            vTaskDelay(pdMS_TO_TICKS(250));
-        break;
-        case 3:                             //BACK
-            motores.Update(TB6612::RIGHT);    
-            vTaskDelay(pdMS_TO_TICKS(500));
-        break;
-        default:
-        break;
-    }
-
-    while (true)
-    {
-    //    ESP_LOGI(TAG,"CORE_A: SEARCHING");
-        if (!STATUS_TOF) 
+        while(key_status)
         {
-            motores.Update(TB6612::FWD);
-            Leds_update(LED::OFF, LED::OFF, LED::OFF, LED::OFF);
-        } 
-        if (CheckSensorLinea(sensor_linea_i))
-        {
-            motores.Update(TB6612::RIGHT);
-            Leds_update(LED::OFF, LED::OFF, LED::ON, LED::OFF);
-            vTaskDelay(pdMS_TO_TICKS(tiempo_de_giro));
+            if(!key_push)
+                {
+                    ref_time = esp_timer_get_time();
+                }
+            key_time = esp_timer_get_time();
+            if (key_time - ref_time > 3000000)
+                {
+                if(selected_start_mode == 0){led_left.Update(LED::ON);}   else {led_left.Update(LED::BLINK_250ms);}
+                if(selected_start_mode == 1){led_front.Update(LED::ON);}   else {led_front.Update(LED::BLINK_250ms);}
+                if(selected_start_mode == 2){led_right.Update(LED::ON);}   else {led_right.Update(LED::BLINK_250ms);}
+                if(selected_start_mode == 3){led_back.Update(LED::ON);}   else {led_back.Update(LED::BLINK_250ms);}
+                start = 1;
+                }
+            key_push = 1;
+            key_status = gpio_get_level(PIN_KEY);
         }
-        else if (CheckSensorLinea(sensor_linea_d))
+
+        if (key_push && (key_time - ref_time < 3000000))
         {
-            motores.Update(TB6612::LEFT);
-            Leds_update(LED::ON, LED::OFF, LED::OFF, LED::OFF);
-            vTaskDelay(pdMS_TO_TICKS(tiempo_de_giro));
+            key_push = 0;
+            selected_start_mode ++;
+            if (selected_start_mode > 3){selected_start_mode = 0;};
+            switch (selected_start_mode)
+            {
+            case 0:
+                Leds_update(LED::ON, LED::OFF, LED::OFF, LED::OFF);
+            break;
+            case 1:
+                Leds_update(LED::OFF, LED::ON, LED::OFF, LED::OFF);
+            break;
+            case 2:
+                Leds_update(LED::OFF, LED::OFF, LED::ON, LED::OFF);
+            break;
+            case 3:
+                Leds_update(LED::OFF, LED::OFF, LED::OFF, LED::ON);
+            break;
+            default:
+            break;
+            }
+            ESP_LOGE(TAG, "selected_start_mode: %u", selected_start_mode);
+
+        }
+    
+        while (start)
+        {
+            ESP_LOGE(TAG, "START!");
+            vTaskDelay(pdMS_TO_TICKS(tiempo_espera_inicio));    //5seg
+            ESP_LOGE(TAG, "GO!");
+            switch(selected_start_mode)
+            {  
+                case 0:                             //LEFT
+                    motores.Update(TB6612::RIGHT);     
+                    vTaskDelay(pdMS_TO_TICKS(250));
+                break;
+                case 1:                             //FRONT
+                    motores.Update(TB6612::FWD);     
+                break;
+                case 2:                             //RIGHT
+                    motores.Update(TB6612::LEFT);     
+                    vTaskDelay(pdMS_TO_TICKS(250));
+                break;
+                case 3:                             //BACK
+                    motores.Update(TB6612::RIGHT);    
+                    vTaskDelay(pdMS_TO_TICKS(500));
+                break;
+                default:
+                break;
+            }
+
+            while(1)
+            {
+                if (!STATUS_TOF)
+                {
+                    motores.Update(TB6612::FWD);
+                    Leds_update(LED::OFF, LED::OFF, LED::OFF, LED::OFF);
+                } 
+                if (CheckSensorLinea(sensor_linea_i))
+                {
+                    motores.Update(TB6612::RIGHT);
+                    Leds_update(LED::OFF, LED::OFF, LED::ON, LED::OFF);
+                    vTaskDelay(pdMS_TO_TICKS(tiempo_de_giro));
+                }
+                else if (CheckSensorLinea(sensor_linea_d))
+                {
+                    motores.Update(TB6612::LEFT);
+                    Leds_update(LED::ON, LED::OFF, LED::OFF, LED::OFF);
+                    vTaskDelay(pdMS_TO_TICKS(tiempo_de_giro));
+                }
+            }
         }
     }
 }
@@ -268,40 +292,10 @@ void coreBThread(void *arg)
             else{
                 STATUS_TOF = 0;
             }
-
+ //           ESP_LOGE(TAG, "tof_C: %u\t tof_i: %u\t tof_d: %u", CheckSensorTOF(sensor_distancia_c,1), CheckSensorTOF(sensor_distancia_i,0), CheckSensorTOF(sensor_distancia_d,2));
         }
     }
 
-}
-
-
-void i2c_scanner()
-{
-  //    CheckSensorTOF(sensor_distancia_i);
-        int i;
-        esp_err_t espRc;
-        printf("     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f\n");
-        printf("00:         ");
-        for (i=3; i< 0x78; i++) {
-            i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-            i2c_master_start(cmd);
-            i2c_master_write_byte(cmd, (i << 1) | I2C_MASTER_WRITE, 1 /* expect ack */);
-            i2c_master_stop(cmd);
-
-            espRc = i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
-            if (i%16 == 0) {
-                printf("\n%.2x:", i);
-            }
-            if (espRc == 0) {
-                printf(" %.2x", i);
-            } else {
-                printf(" --");
-            }
-            //ESP_LOGD(tag, "i=%d, rc=%d (0x%x)", i, espRc, espRc);
-            i2c_cmd_link_delete(cmd);
-	}
-	printf("\n");
-	vTaskDelete(NULL);
 }
 
 
@@ -309,18 +303,10 @@ void app_main()
 {   
     ESP_LOGE(TAG, "Iniciando software");
     Init();
+    vTaskDelay(pdMS_TO_TICKS(1000));
     Init_VL53L0X();
-
-    while(0)
-    {
-    //    CheckSensorTOF(sensor_distancia_i,0);
-    //    CheckSensorTOF(sensor_distancia_c,1);
-    //    CheckSensorTOF(sensor_distancia_d,2);
-
-        i2c_scanner();
-    }
-
-
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    Init_Key();
 
     xTaskCreatePinnedToCore(coreBThread, "core_B", 4096, NULL, 9, &core_B, 1);    
     xTaskCreatePinnedToCore(coreAThread, "core_A", 4096, NULL, 10, &core_A, 0);
